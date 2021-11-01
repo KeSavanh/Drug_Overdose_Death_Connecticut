9e9a8397fbd3d34134a556e7a51e8341af4b535e
Your Name <you@example.com>
2021-10-31 21:23:59 -0700

    working on drug_type

--- data_schema.sql
+++ data_schema.sql
@@ -1,11 +1,11 @@
 DROP TABLE IF EXISTS census_ct;
-DROP TABLE IF EXISTS drug_overdose;
-
+DROP TABLE IF EXISTS death_overdose;
+DROP TABLE IF EXISTS drug_type;
 
 
 CREATE TABLE census_ct(
        id INT PRIMARY KEY, 
-		year FLOAT,
+		year DATE,
 	 	county VARCHAR,
 	 	household_Income FLOAT,
 		population FLOAT,
@@ -20,18 +20,52 @@ CREATE TABLE census_ct(
 		poverty_count FLOAT,
 	    poverty_rate FLOAT,
         uneducated_Rate FLOAT);
-		
-CREATE TABLE drug_overdose(
+
+
+CREATE TABLE death_overdose(
 		id INT PRIMARY KEY,
-		drugid	VARCHAR,
-		date	DATE,
+		date DATE,
+		age FLOAT,
 		sex	VARCHAR,
-		race	VARCHAR,
-		cause_of_death VARCHAR,
+		race	VARCHAR,	
 		city	VARCHAR,
 		county VARCHAR,
-		state VARCHAR
+		state VARCHAR,
+		manner_of_death VARCHAR,
+		cause_of_death VARCHAR
+		);
+
+CREATE TABLE drug_type(
+		id INT PRIMARY KEY,
+		date DATE,
+		age FLOAT,
+		sex	VARCHAR,
+		race	VARCHAR,	
+		city	VARCHAR,
+		county VARCHAR,
+		state VARCHAR,
+		manner_of_death VARCHAR,
+		cause_of_death VARCHAR,
+		heroin VARCHAR,
+		cocaine VARCHAR,
+		fentanyl VARCHAR,
+		fentanyl_analogue VARCHAR,
+       	oxycodone VARCHAR,
+		oxymorphone VARCHAR,
+	 	ethanol VARCHAR,
+		hydrocodone VARCHAR,
+		benzodiazepine VARCHAR,
+        methadone VARCHAR,
+		amphet VARCHAR,
+		tramad VARCHAR,
+		morphine_not_heroin VARCHAR,
+		hydromorphone VARCHAR,
+        xylazine VARCHAR,
+		other VARCHAR,
+		opiate_nos VARCHAR,
+		any_opioid VARCHAR
 		);
        
 SELECT * FROM census_ct;
-SELECT * FROM drug_overdose;
+SELECT * FROM death_overdose;
+SELECT * FROM drug_type;
